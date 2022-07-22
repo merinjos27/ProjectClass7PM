d0d7943fe4c83d236bbd0a1afefea32be38e81a8
merinjos27 <meringreenomr@gmail.com>
2022-07-22 19:35:58 +0530

    First Time Push

---
+++ src/test/java/com/gitclass/ClassA.java
@@ -0,0 +1,21 @@
+package com.gitclass;
+
+public class ClassA {
+	
+	private void method1() {
+		System.out.println("1");
+
+	}
+	
+	
+	private void method2() {
+		System.out.println("2");
+
+	}
+	
+	private void method3() {
+		System.out.println("3");
+
+	}
+
+}
